e13810d1f265064ae91a2e31cc7bbccbcc94e909
Екатерина Пожидаева <kateworks@yandex.ru>
2020-09-16 18:18:17 +0300

    Updates

---
+++ precache-manifest.a1d26138458060282cd7cd6a82090f62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d95f6235d0e14b5576b3441d2e4db0d",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "c5be39a0d2601f27450b",
+    "url": "/react-todo-list/static/css/main.13591dd1.chunk.css"
+  },
+  {
+    "revision": "30ad514ec055f151c872",
+    "url": "/react-todo-list/static/js/2.3d9d6c03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.3d9d6c03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5be39a0d2601f27450b",
+    "url": "/react-todo-list/static/js/main.0ca5e453.chunk.js"
+  },
+  {
+    "revision": "9f30537d964e7dd1ed06",
+    "url": "/react-todo-list/static/js/runtime-main.59df3b9a.js"
+  },
+  {
+    "revision": "cbf6c23bb810563e477f439026b37e55",
+    "url": "/react-todo-list/static/media/btn-copy.cbf6c23b.svg"
+  },
+  {
+    "revision": "f41e53b6df66b866a8f41fa52c389e26",
+    "url": "/react-todo-list/static/media/btn-delete.f41e53b6.svg"
+  },
+  {
+    "revision": "66847d3144ff86eeeb9f0a67c2cbcb5c",
+    "url": "/react-todo-list/static/media/btn-edit.66847d31.svg"
+  }
+]);
